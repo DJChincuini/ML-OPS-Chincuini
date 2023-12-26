6f94ec5c40120e30542470db79099a3152f79acb
DJChincuini <daantechincuini42@gmail.com>
2023-12-26 17:40:51 -0300

    Etiquetas

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = FastAPI()
 
 @app.get("/PlayTimeGenre/{genero}")
 def PlayTimeGenre(genero:str):
+    '''
+    Debe devolver año con mas horas jugadas para dicho género.
+    Ejemplo de retorno: {"Año de lanzamiento con más horas jugadas para Género X" : 2013}
+    '''
     try:
         return func_playTime(genero)
     except Exception as e:
